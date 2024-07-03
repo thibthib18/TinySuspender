f5d298a905019398e76589d7659076277ccc636c
thib <thibault@seervision.com>
2024-07-03 12:58:19 +0400

    Update key bindings

--- src/tiny-suspender/js/suspend.js
+++ src/tiny-suspender/js/suspend.js
@@ -1,25 +1,25 @@
-
 let suspendUrl = new URL(location.href);
-let pageUrl = suspendUrl.searchParams.get('url');
-let favIconUrl = suspendUrl.searchParams.get('favIconUrl');
-let title = suspendUrl.searchParams.get('title');
-let darkMode = suspendUrl.searchParams.get('dark_mode') === 'true';
+let pageUrl = suspendUrl.searchParams.get("url");
+let favIconUrl = suspendUrl.searchParams.get("favIconUrl");
+let title = suspendUrl.searchParams.get("title");
+let darkMode = suspendUrl.searchParams.get("dark_mode") === "true";
 
 // compatibility with previous version
 // will be removed in the next version
 if (!pageUrl) {
   let suspendUrl = new URL(location.href);
-  let hash = suspendUrl.hash ? suspendUrl.hash.replace('#', '') : '';
+  let hash = suspendUrl.hash ? suspendUrl.hash.replace("#", "") : "";
 
   let hashParams = {};
   let e,
-      a = /\+/g,  // Regex for replacing addition symbol with a space
-      r = /([^&;=]+)=?([^&;]*)/g,
-      d = function (s) { return decodeURIComponent(s.replace(a, " ")); },
-      q = hash;
+    a = /\+/g, // Regex for replacing addition symbol with a space
+    r = /([^&;=]+)=?([^&;]*)/g,
+    d = function (s) {
+      return decodeURIComponent(s.replace(a, " "));
+    },
+    q = hash;
 
-  while (e = r.exec(q))
-     hashParams[d(e[1])] = d(e[2]);
+  while ((e = r.exec(q))) hashParams[d(e[1])] = d(e[2]);
 
   pageUrl = hashParams.uri;
   title = hashParams.title;
@@ -41,10 +41,10 @@ document.addEventListener("keypress", function (event) {
 
   if (event.shiftKey === true) {
     switch (event.key) {
-      case "J":
+      case "H":
         nextPrevTab(-1);
         break;
-      case "K":
+      case "L":
         nextPrevTab(1);
         break;
     }
@@ -52,49 +52,48 @@ document.addEventListener("keypress", function (event) {
 });
 
 document.onclick = () => {
-    this.chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-      tabs.forEach((tab) => {
-        this.chrome.runtime.sendMessage({command: "ts_restore_tab", tabId: tab.id});
+  this.chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+    tabs.forEach((tab) => {
+      this.chrome.runtime.sendMessage({
+        command: "ts_restore_tab",
+        tabId: tab.id,
       });
     });
-}
-
+  });
+};
 
 if (pageUrl) {
-  document.querySelector('.title .description').setAttribute('href', pageUrl);
-  document.querySelector('.title .url').setAttribute('href', pageUrl);
-  document.querySelector('.title .url').textContent = pageUrl;
+  document.querySelector(".title .description").setAttribute("href", pageUrl);
+  document.querySelector(".title .url").setAttribute("href", pageUrl);
+  document.querySelector(".title .url").textContent = pageUrl;
 }
 
-
 if (favIconUrl) {
-  var icon = document.createElement('img');
-  icon.setAttribute('src', favIconUrl);
-  document.querySelector('.title .icon').appendChild(icon);
+  var icon = document.createElement("img");
+  icon.setAttribute("src", favIconUrl);
+  document.querySelector(".title .icon").appendChild(icon);
 
-  var link = document.createElement('link');
-  link.type = 'image/x-icon';
-  link.rel = 'shortcut icon';
+  var link = document.createElement("link");
+  link.type = "image/x-icon";
+  link.rel = "shortcut icon";
   link.href = favIconUrl;
-  document.getElementsByTagName('head')[0].appendChild(link);
+  document.getElementsByTagName("head")[0].appendChild(link);
 }
 
 if (title) {
   document.title = title;
-  document.querySelector('.title .description').textContent = title;
+  document.querySelector(".title .description").textContent = title;
 }
 
 if (darkMode) {
-  document.body.classList.add('dark-mode');
+  document.body.classList.add("dark-mode");
 }
 
 chrome.storage.onChanged.addListener((changes, namespace) => {
-  console.log(changes, namespace)
+  console.log(changes, namespace);
   if (changes.dark_mode && changes.dark_mode.newValue) {
-    document.body.classList.add('dark-mode');
-  }
-  else {
-    document.body.classList.remove('dark-mode');
+    document.body.classList.add("dark-mode");
+  } else {
+    document.body.classList.remove("dark-mode");
   }
 });
-
